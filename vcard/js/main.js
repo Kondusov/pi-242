8615f1606cc566dc40c4136e9a47dacefd5dc2c8
Kondusov <kondusov.ru@gmail.com>
2025-03-05 13:07:12 +0300

    Добавили попап окно при попытке уйти с сайта, слушаем событие mouseleave

---
+++ vcard/js/main.js
@@ -0,0 +1,53 @@
+
+const windowInnerWidth = document.documentElement.clientWidth;
+const scrollbarWidth = parseInt(window.innerWidth) - parseInt(document.documentElement.clientWidth);
+
+const bodyElementHTML = document.getElementsByTagName("body")[0];
+const modalBackground = document.getElementsByClassName("modalBackground")[0];
+const modalClose = document.getElementsByClassName("modalClose")[0];
+const modalActive = document.getElementsByClassName("modalActive")[0];
+
+// document.getElementById("html").addEventListener(
+//     "mouseleave",
+//     function (event) {
+//         alert("Не уходи, постой...")
+//     }
+// );
+// функция для корректировки положения body при появлении ползунка прокрутки
+function bodyMargin() {
+    bodyElementHTML.style.marginRight = "-" + scrollbarWidth + "px";
+}
+
+// при длинной странице - корректируем сразу
+bodyMargin();
+// событие нажатия на триггер открытия модального окна
+document.getElementById("html").addEventListener(
+    "mouseleave", function () {
+    // делаем модальное окно видимым
+    modalBackground.style.display = "block";
+
+    // если размер экрана больше 1366 пикселей (т.е. на мониторе может появиться ползунок)
+    if (windowInnerWidth >= 1366) {
+        bodyMargin();
+    }
+
+    // позиционируем наше окно по середине, где 175 - половина ширины модального окна
+    modalActive.style.left = "calc(20%)";
+});
+// нажатие на крестик закрытия модального окна
+modalClose.addEventListener("click", function () {
+    modalBackground.style.display = "none";
+    if (windowInnerWidth >= 1366) {
+        bodyMargin();
+    }
+});
+
+// закрытие модального окна на зону вне окна, т.е. на фон
+modalBackground.addEventListener("click", function (event) {
+    if (event.target === modalBackground) {
+        modalBackground.style.display = "none";
+        if (windowInnerWidth >= 1366) {
+            bodyMargin();
+        }
+    }
+});
